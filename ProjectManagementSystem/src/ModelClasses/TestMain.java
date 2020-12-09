ee37b1ecc14e49cfe3ed37a86dc134f585af32cf
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2020-12-09 14:11:12 +0100

    Add files via upload

---
+++ ProjectManagementSystem/src/ModelClasses/TestMain.java
@@ -0,0 +1,11 @@
+package ModelClasses;
+
+public class TestMain {
+
+
+  public static void main(String[] args) {
+    System.out.println("Hello Darkness");
+
+    ProjectList wick = new ProjectList();
+  }
+}
