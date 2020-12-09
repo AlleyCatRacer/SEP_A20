01222cc2a8c1f161db302f0444a6d7142d26c27b
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2020-12-09 13:22:02 +0100

    Update Role.java

--- ProjectManagementSystem/src/ModelClasses/Role.java
+++ ProjectManagementSystem/src/ModelClasses/Role.java
@@ -5,7 +5,7 @@ enum Role
     TEAM_MEMBER("Team member"), SCRUM_MASTER("Scrum master"), PRODUCT_OWNER("Product owner");
     private String role;
 
-    Role(String role)
+    private Role(String role)
     {
         this.role = role;
     }
