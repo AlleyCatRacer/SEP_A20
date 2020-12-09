1ac3321e441eaf2e94eee2852dacd5b58624d433
AlleyCatRacer <74299218+AlleyCatRacer@users.noreply.github.com>
2020-12-09 13:19:25 +0100

    Update Role.java

--- ProjectManagementSystem/src/ModelClasses/Role.java
+++ ProjectManagementSystem/src/ModelClasses/Role.java
@@ -1,5 +1,17 @@
 package ModelClasses;
 
-public enum Role {
-    TeamMember, ProjectManager, ScrumMaster;
+enum Role
+{
+    TEAM_MEMBER("Team member"), SCRUM_MASTER("Scrum master"), PRODUCT_OWNER("Product owner");
+    private String role;
+
+    Role(String role)
+    {
+        this.role = role;
+    }
+
+    private boolean isRole(String role)
+    {
+        return role.equalsIgnoreCase(TEAM_MEMBER.role) || role.equalsIgnoreCase(SCRUM_MASTER.role) || role.equalsIgnoreCase(PRODUCT_OWNER.role);
+    }
 }
