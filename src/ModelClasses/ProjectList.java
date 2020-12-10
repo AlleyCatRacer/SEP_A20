42fb985e797d64ad7df4459810246ee0f2916915
Lily <tothlilizs@users.noreply.github.com>
2020-12-10 17:02:42 +0100

    project & projectlist
    haleluja

--- src/ModelClasses/ProjectList.java
+++ src/ModelClasses/ProjectList.java
@@ -3,27 +3,52 @@ package ModelClasses;
 import java.util.ArrayList;
 
 public class ProjectList
-{    //sorry haven't started this yet
-  private ArrayList<Project> projects;
+{
+  public ArrayList<Project> projects;
   public ProjectList(){
     projects = new ArrayList<>();
   }
   public Project getProjectByTitle(String title){
-    return null;
+    int index = 0;
+    for (Project e : projects)
+    {
+      if (e.getTitle() == title)
+      {
+        index = projects.indexOf(e);
+      }
+
+    }
+
+    return projects.get(index);
   }
   public Project getEndedProjectByTitle(String title){
-    return null;
+    int index = 0;
+    for (Project e : projects)
+    {
+      if (e.getTitle().equals(title) & e.getCurrentStatus().equals(Status.WAITING.getStatusString()))
+      {
+        index = projects.indexOf(e);
+      }
+
+    }
+
+    return projects.get(index);
   }
   public void addProject(Project project){
       projects.add(project);
   }
   public void removeProject(String title){
-
+    projects.forEach((e) -> {
+      if (e.getTitle().equals(title)){
+        projects.remove(e);
+      }
+    });
   }
-  public ArrayList<Project> getAllProjects(Status status){
 
-    return null;
-}
+  public ArrayList<Project> getAllProjects()
+  {
+    return projects;
+  }
 
   @Override public String toString()
   {
