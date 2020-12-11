bf90cbd82734079f5ec396593c96d95a4bd6b808
YoyoMy <35624284+YoyoMy@users.noreply.github.com>
2020-12-11 08:21:39 +0100

    Adding some files, they are not finished

---
+++ ProjectManager.java
@@ -0,0 +1,12 @@
+package model;
+
+public class ProjectManager  implements ProjectModel{
+    private Team team;
+    private ProjectList projectList;
+    public ProjectManager()
+    {
+        this.team = new Team();
+        this.projectList = new ProjectList();
+    }
+    public
+}
