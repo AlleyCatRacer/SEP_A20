b70c18e5ffaf7c8b315b93bcc93760f46da98f09
AlleyCatRacer <AlleyCatRacer@users.noreply.github.com>
2020-12-11 10:23:02 +0100

    Yoana

---
+++ src/ModelClasses/ProjectManager.java
@@ -0,0 +1,12 @@
+package ModelClasses;
+
+public class ProjectManager  implements ProjectModel{
+    private Team team;
+    private ProjectList projectList;
+    public ProjectManager()
+    {
+        this.team = new Team();
+        this.projectList = new ProjectList();
+    }
+    public
+}
---
+++ src/MyApplication.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+import model.ProjectManager;
+import model.ProjectModel;
+import model.Project;
+import view.ViewHandler;
+
+public class MyApplication extends Application
+{
+    public void start(Stage primaryStage)
+    {
+        ProjectManager model = new ProjectManager();
+        ViewHandler view = new ViewHandler(model);
+        view.start(primaryStage);
+    }
+}
